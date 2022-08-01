68f1bd1f0b989c8aa232433189a77d7ade8de6c4
Kwang <gukwanglim@naver.com>
2022-08-01 16:30:40 +0900

    ring_9

--- 기업연계/login/app.py
+++ 기업연계/login/app.py
@@ -1,11 +1,14 @@
 from datetime import datetime
+from itertools import count
  
 from flask import Flask, render_template, session, g, request, url_for, flash, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
-from forms import UserCreateForm, UserLoginForm
+from sqlalchemy import select
+
+from forms import ProductInputForm, UserCreateForm, UserLoginForm
 
 # bp = Blueprint('main', __name__, url_prefix='/main')
 
@@ -17,9 +20,77 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db = SQLAlchemy(app)
 
+# 사용자 모델 추가
+class User(db.Model):
+    __table_name__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, email, password):
+      self.username = username
+      self.email = email
+      self.password = password
+
+    def __repr__(self):
+        return f"<User('{self.id}', '{self.username}', '{self.email}')>"
+
+# 제품 모델 추가
+class Product(db.Model):
+    __table_name__ = 'product'
+
+    prod_id = db.Column(db.Integer, primary_key=True)
+    prod_name = db.Column(db.String(150), unique=False, nullable=True)
+    prod_image = db.Column(db.Text(), unique=False, nullable=True)
+    prod_price = db.Column(db.String(150), unique=False, nullable=True)
+    prod_quantity = db.Column(db.String(150), unique=False, nullable=True)
+    prod_expand = db.Column(db.String(300), unique=False, nullable=True)
+
+    def __init__(self, prod_name, prod_image, prod_price, prod_quantity, prod_expand):
+        self.prod_name = prod_name
+        self.prod_image = prod_image
+        self.prod_price = prod_price
+        self.prod_quantity = prod_quantity
+        self.prod_expand = prod_expand
+
+    def __repr__(self):
+        return f"<Product('{self.prod_id}', '{self.prod_name}', '{self.prod_image}', '{self.prod_price}', '{self.prod_quantity}', '{self.prod_expand}')>"
+
+
 @app.route('/')
 def main():
-    return render_template('main/main.html')
+    prod = Product.query.all()
+
+
+# flask는 len 사용 불가. {% for i in range(prod | length) %} 이런 식으로 사용 가능.
+    # prod_list = []
+
+    # for i in range(a):
+        
+    #     prod_list.append(prod[i].prod_image)
+    #     return prod_list
+
+    # print('여기에 출력 : ', len(prod))      
+
+
+    # [<Product('1', '사과', 'http://kormedi.com/wp-content/uploads/2020/09/roman-samokhin-580x429.jpg', '1500', 'None', '달고 맛있습니다.')>, <Product('2', '아포카도', 'https://img.etoday.co.kr/pto_db/2021/06/600/20210602131530_1627835_1200_779.jpg', '9000', 'None', '잘 익은 아포카도.')>]
+
+
+    # print('////////')
+    # print('그 다음 출력 : ', prod[0])
+
+    # [<Product('1', '사과', 'http://kormedi.com/wp-content/uploads/2020/09/roman-samokhin-580x429.jpg', '1500', 'None', '달고 맛있습니다.')>]
+
+
+    # print('////////////')
+    # print('제발 : ', prod[0].prod_name)
+
+
+    # 사과
+
+    return render_template('main/main.html', prod=prod)
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -42,9 +113,11 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = UserLoginForm()
-    if request.method == 'POST' and form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():                 # 점검
         error = None
+        # print("비밀번호:%s" % form.password.data) 
         user = User.query.filter_by(username=form.username.data).first()
+        # print("아이디:%s" % form.username.data) 
         if not user:
             error = "존재하지 않는 사용자입니다."
         elif not check_password_hash(user.password, form.password.data):
@@ -52,6 +125,8 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user.id
+            # print(session)
+            session['user_name'] = form.username.data
             return redirect(url_for('main'))
         flash(error)
     return render_template('auth/login.html', form=form)
@@ -73,28 +148,13 @@ def logout():
 def account():
     return render_template('auth/my-account.html')
 
-# 사용자 모델 추가
-class User(db.Model):
-    __table_name__ = 'user'
+@app.route('/upload')
+def up():
+    return render_template('page/upload.html')
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+@app.route('/product')
+def prod():
 
-    def __init__(self, username, email, password):
-      self.username = username
-      self.email = email
- 
-      self.set_password(password)
-
-    def __repr__(self):
-        return f"<User('{self.id}', '{self.username}', '{self.email}')>"
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
- 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+    return render_template('shop/shop-page.html')
 
     
--- 기업연계/login/forms.py
+++ 기업연계/login/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField
+from wtforms import StringField, PasswordField, EmailField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class UserCreateForm(FlaskForm):
@@ -12,3 +12,15 @@ class UserCreateForm(FlaskForm):
 class UserLoginForm(FlaskForm):
     username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
     password = PasswordField('비밀번호', validators=[DataRequired()])
+
+class ProductInputForm(FlaskForm):
+    prod_name = StringField('제품명', validators=[Length(min=3, max=25)])
+    prod_image = TextAreaField('제품 사진', validators=None)
+    prod_price = StringField('제품 가격', validators=[Length(min=3, max=25)])
+    prod_quantity = StringField('제품 수량', validators=None)
+    prod_expand = StringField('제품 소개', validators=[Length(min=3, max=300)])
+
+
+
+
+# https://wtforms.readthedocs.io/en/2.3.x/fields/#basic-fields 참고  FileField 혹은 MultipleFileField 확인
